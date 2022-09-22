a0b9550fdcdcb7781120ff8d9485a16fcce586a9
ryeluvolu <106367531+ryeluvolu@users.noreply.github.com>
2022-09-22 00:35:14 -0500

    fixed typo in a comment

--- src/functions/ValueReturnMethod.java
+++ src/functions/ValueReturnMethod.java
@@ -14,7 +14,7 @@ public class ValueReturnMethod {
 		String name = returnMyName();
 		System.out.println(name);
 
-		// Line# 19 equivalent to line#14 and #15. Preferred coding practice is line#17
+		// Line# 19 equivalent to line#14 and #15. Preferred coding practice is line#19
 		// because its less lines of code.
 		System.out.println(returnMyName());
 	}
